e852f90c56e6d9c733ea48f9aa0e33dc51e4f26d
PriyankaKorat <priyankakorat.at@gmail.com>
2024-04-27 00:40:47 +1000

    My handwritten homework weeek 5

---
+++ src/homework_week_5/Programme_8_DivisionOfTwoNumbers.java
@@ -0,0 +1,17 @@
+package homework_week_5;
+
+public class Programme_8_DivisionOfTwoNumbers {
+    /**
+     * 8. Write a Java program to divide two numbers and print on the screen.
+     * Test Data: 50/3
+     * Expected Output: 16
+     */
+    public static void main(String[] args) {
+        int value1 = 50;
+        int value2 = 3;
+        int division = value1 / value2;
+        System.out.println("The " + value1 + " is divided by " + value2 + " the answer is " + division);
+    }
+}
+
+
